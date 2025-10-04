78aa3d0e0501e9a9b96ef0f5e1beabc182344bc2
maycolvasquez-iud <maycolvasquez@est.iudigital.edu.co>
2025-10-03 21:40:31 -0500

    fix: Agregar trailingSlash para resolver error Not Found en Render

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configuración para producción
+  trailingSlash: true,
   env: {
     NEXT_PUBLIC_API_URL: 'https://localaid-backend-1.onrender.com/api'
   }
