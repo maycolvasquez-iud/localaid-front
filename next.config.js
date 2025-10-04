78de3cf9759fe14302acd76f262a3f45684e8a60
maycolvasquez-iud <maycolvasquez@est.iudigital.edu.co>
2025-10-03 22:07:03 -0500

    fix: Corregir configuración para Static Site en Render

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  distDir: 'out',
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   env: {
     NEXT_PUBLIC_API_URL: 'https://localaid-backend-1.onrender.com/api'
   }
